92c732f782c121bc08fed50cc83997e7a0d6055b
Cuyqo <saon0410@gmail.com>
2026-08-24 18:40:30 +0900

    feat: BaseEntity에 soft delete(deletedAt) 추가

--- src/main/java/com/iitsaii/photobooth/global/entity/BaseEntity.java
+++ src/main/java/com/iitsaii/photobooth/global/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
@@ -13,4 +14,18 @@ public abstract class BaseEntity extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private LocalDateTime deletedAt;
+
+    public boolean isDeleted() {
+        return deletedAt != null;
+    }
+
+    public void delete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public void restore() {
+        this.deletedAt = null;
+    }
 }
