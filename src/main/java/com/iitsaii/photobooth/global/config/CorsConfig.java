311e68490a8029a25b1c6270eead6246e4e5feb6
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-09-01 05:47:24 +0900

    chore: CORS 설정 파일 추가

---
+++ src/main/java/com/iitsaii/photobooth/global/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.iitsaii.photobooth.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "https://itsai.co.kr"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
